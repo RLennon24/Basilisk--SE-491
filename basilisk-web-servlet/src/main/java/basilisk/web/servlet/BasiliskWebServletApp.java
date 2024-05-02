05fd347681e9ddca46930cd86ee0e4e903fe46f3
Zanco2022 <100976175+Zanco2022@users.noreply.github.com>
2024-05-02 13:31:27 -0700

    Update BasiliskWebServletApp.java

--- basilisk-web-servlet/src/main/java/basilisk/web/servlet/BasiliskWebServletApp.java
+++ basilisk-web-servlet/src/main/java/basilisk/web/servlet/BasiliskWebServletApp.java
@@ -1,8 +1,11 @@
 package basilisk.web.servlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class BasiliskWebServletApp {
 
     public static void main(String[] args) {
+        SpringApplication.run(BasiliskWebServletApp.class, args);
 
     }
 }
