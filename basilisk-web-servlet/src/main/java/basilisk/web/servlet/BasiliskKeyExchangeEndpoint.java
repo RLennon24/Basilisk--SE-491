33e54cad11b833164f2d0992ebf3250af1022e41
zach <zwile@depaul.edu>
2024-05-05 22:09:25 +0000

    Added to BasiliskKeyExchangeEndpoint.java

--- basilisk-web-servlet/src/main/java/basilisk/web/servlet/BasiliskKeyExchangeEndpoint.java
+++ basilisk-web-servlet/src/main/java/basilisk/web/servlet/BasiliskKeyExchangeEndpoint.java
@@ -1,1 +1,12 @@
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@RestController
+public class BasiliskKeyExchangeEndpoint {
+	
+	@GetMapping("/exchange-keys")
+	public String keyExchange (@RequestParam(value = "thePublicKey") String thePublicKey) {
+		return "Got the public key: " + thePublicKey;
+	}
+}
