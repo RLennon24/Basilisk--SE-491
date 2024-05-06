db61b37687da15bcff825e487012428f48edbd1e
zach <zwile@depaul.edu>
2024-05-06 03:48:47 +0000

    Added Endpoint file

---
+++ basilisk-web-servlet/src/main/java/basilisk/web/servlet/BasiliskKeyExchangeEndpoint.java
@@ -0,0 +1,10 @@
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasiliskKeyExchangeEndpoint {
+	public String keyExchange (@RequestParam(value = "thePublicKey") String thePublicKey) {
+		return "Got the public key: " + thePublicKey;
+	}
+}
+
