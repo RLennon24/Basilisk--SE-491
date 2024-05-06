5e5e24ed08b02311a5cc16d2eee37878477da3fb
zach <zwile@depaul.edu>
2024-05-06 03:01:21 +0000

    Took out mapping

--- basilisk-web-servlet/src/main/java/basilisk/web/servlet/BasiliskKeyExchangeEndpoint.java
+++ basilisk-web-servlet/src/main/java/basilisk/web/servlet/BasiliskKeyExchangeEndpoint.java
@@ -1,11 +1,9 @@
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class BasiliskKeyExchangeEndpoint {
 	
-	@GetMapping("/exchange-keys")
 	public String keyExchange (@RequestParam(value = "thePublicKey") String thePublicKey) {
 		return "Got the public key: " + thePublicKey;
 	}
