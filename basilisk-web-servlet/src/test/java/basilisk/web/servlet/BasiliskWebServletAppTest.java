9caded422cca84e1bf339b7b40e15c1057552f26
zach <zwile@depaul.edu>
2024-05-08 18:26:30 +0000

    Added some imports to Test

--- basilisk-web-servlet/src/test/java/basilisk/web/servlet/BasiliskWebServletAppTest.java
+++ basilisk-web-servlet/src/test/java/basilisk/web/servlet/BasiliskWebServletAppTest.java
@@ -1,5 +1,19 @@
 package basilisk.web.servlet;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
 public class BasiliskWebServletAppTest {
-    
+	@Test
+	public void test1() {
+
+	}
+	    
 }
