177823288be2da813fff65f6c273cf3ee82a6702
zach <zwile@depaul.edu>
2024-05-09 19:39:17 +0000

    Added package

--- basilisk-web-servlet/src/main/java/basilisk/web/servlet/BasiliskKeyExchangeEndpoint.java
+++ basilisk-web-servlet/src/main/java/basilisk/web/servlet/BasiliskKeyExchangeEndpoint.java
@@ -1,3 +1,5 @@
+package basilisk.web.servlet; 
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
