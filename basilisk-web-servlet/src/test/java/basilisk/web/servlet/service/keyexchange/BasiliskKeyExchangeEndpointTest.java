6b6da820a400d187e32d63eb0b742f21f1b03082
Zanco2022 <100976175+Zanco2022@users.noreply.github.com>
2024-05-14 02:09:07 -0700

    Update BasiliskKeyExchangeEndpointTest.java

--- basilisk-web-servlet/src/test/java/basilisk/web/servlet/service/keyexchange/BasiliskKeyExchangeEndpointTest.java
+++ basilisk-web-servlet/src/test/java/basilisk/web/servlet/service/keyexchange/BasiliskKeyExchangeEndpointTest.java
@@ -1,13 +1,19 @@
 package basilisk.web.servlet.service.keyexchange;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.mockito.Mockito.*;
+import basilisk.web.servlet.keygen.KeyGenerator;
+import java.security.PublicKey;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BasiliskKeyExchangeEndpoint.class)
@@ -16,30 +22,18 @@ public class BasiliskKeyExchangeEndpointTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private KeyGenerator keyGenerator;
-    @MockBean
-    private KeyCache keyCache;
-
     @Test
     public void testExchangeKey() throws Exception {
-        PublicKey mockPublicKey = mock(PublicKey.class);
-        when(keyGenerator.getPublicKey()).thenReturn(mockPublicKey);
-        
-        mockMvc.perform(MockMvcRequestBuilders.post("/exchange-keys")
-                .content("{\"publicKey\":\"ClientPublicKey\"}")
-                .contentType(MediaType.APPLICATION_JSON))
+        try (MockedStatic<KeyGenerator> mockedKeyGenerator = Mockito.mockStatic(KeyGenerator.class)) {
+            PublicKey mockPublicKey = mock(PublicKey.class);
+            mockedKeyGenerator.when(KeyGenerator::getPublicKey).thenReturn(mockPublicKey);
+
+            mockMvc.perform(MockMvcRequestBuilders.post("/exchange-keys")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"publicKey\":\"examplePublicKey\"}"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.serverPublicKey").exists());
-    }
-
-    @Test
-    public void getTestData() throws Exception {
-        when(keyCache.getData("1")).thenReturn("Data for ID: 1");
-        
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/data/1")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Data for ID: 1"));
+        }
     }
 }
+
