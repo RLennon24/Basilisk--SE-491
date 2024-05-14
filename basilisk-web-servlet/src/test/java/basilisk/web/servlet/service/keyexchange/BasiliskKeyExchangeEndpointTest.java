b91481f31a952947ba18f3e408f34143fcadc171
zach <zwile@depaul.edu>
2024-05-14 02:43:51 +0000

    Changed parts of BasiliskKeyExchangeEndpointTest

--- basilisk-web-servlet/src/test/java/basilisk/web/servlet/service/keyexchange/BasiliskKeyExchangeEndpointTest.java
+++ basilisk-web-servlet/src/test/java/basilisk/web/servlet/service/keyexchange/BasiliskKeyExchangeEndpointTest.java
@@ -1,32 +1,44 @@
 package basilisk.web.servlet.service.keyexchange;
 
-import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
+import basilisk.web.servlet.keygen.KeyGenerator;
+import basilisk.web.servlet.keygen.KeyCache;
+import static org.mockito.Mockito.*;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@ExtendWith(SpringExtension.class)
 @WebMvcTest(BasiliskKeyExchangeEndpoint.class)
 //This is to indicate that the test class is specifically for testing the BasiliskWebServletApp class
 //@AutoConfigureMockMvc(addFilters = false)
-public class BasiliskKeyExchangeEndpointTest extends TestCase {
+public class BasiliskKeyExchangeEndpointTest {
 
     @Autowired
     private MockMvc mockMvc;    //The MockMvc gives methods that can be used to test endpoints without a complete server startup
 
+	@MockBean
+	private KeyGenerator keyGenerator;
+	@MockBean
+	private KeyCache keyCache;
+
     @Test //For the POST endpoint handling key Exchange
     public void testExchangeKey() throws Exception {
-        String askForBody = "{\"publicKey\":\"ClientPublicKey\"}";
+	PublicKey mockPublicKey = mock(PublicKey.class);
+	when(keyGenerator.getPublicKey()).thenReturn(mockPublicKey);
+	when(keyCache.getKeyForService("client")).thenReturn(mockPublicKey);
+        String requestBody = "{\"publicKey\":\"ClientPublicKey\"}";
 
         //Simulates a POST request to the api/exchange-keys
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/exchange-keys")
-                        .content(askForBody)
+                        .content(requestBody)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.serverPublicKey").exists());
@@ -35,6 +47,8 @@ public class BasiliskKeyExchangeEndpointTest extends TestCase {
     //Test method for the GET endpoint, then it expects a succesful response status. Then it verifies that the returned content matches "The data for ID: 1"
     @Test
     public void getTestData() throws Exception {
+	PublicKey mockPublicKey = mock(PublicKey.class);
+	when(keyCache.getKeyForService("client")).thenReturn(mockPublicKey);
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/api/data/1")
                         .accept(MediaType.APPLICATION_JSON))
@@ -43,3 +57,4 @@ public class BasiliskKeyExchangeEndpointTest extends TestCase {
 
     }
 }
+
