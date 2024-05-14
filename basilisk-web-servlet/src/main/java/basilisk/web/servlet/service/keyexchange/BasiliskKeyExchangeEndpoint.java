8da965137dd2805e50163159e49c1edae29a40ad
Zanco2022 <100976175+Zanco2022@users.noreply.github.com>
2024-05-14 01:45:07 -0700

    Update BasiliskKeyExchangeEndpoint.java
    Updated the get

--- basilisk-web-servlet/src/main/java/basilisk/web/servlet/service/keyexchange/BasiliskKeyExchangeEndpoint.java
+++ basilisk-web-servlet/src/main/java/basilisk/web/servlet/service/keyexchange/BasiliskKeyExchangeEndpoint.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import java.security.PublicKey;
 import basilisk.web.servlet.keygen.KeyGenerator;
 import basilisk.web.servlet.keygen.KeyCache;
@@ -19,21 +20,20 @@ public class BasiliskKeyExchangeEndpoint {
     @PostMapping("/exchange-keys")
     public ResponseEntity<Map<String, Object>> exchangeKeys(@RequestBody Map<String, String> requestData) {
         Map<String, Object> response = new HashMap<>();
-        PublicKey publicKey = (PublicKey) KeyGenerator.getPublicKey();  // Casting as safely assumed to be PublicKey
-        response.put("serverPublicKey", publicKey.toString());  // Assuming toString gives a suitable representation
+        PublicKey publicKey = KeyGenerator.getPublicKey();  // Get the public key
+        response.put("serverPublicKey", publicKey.toString());
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/data")
-    public ResponseEntity<String> getData(@RequestParam("id") String id) {
+    public ResponseEntity<String> getData(@RequestParam("id") String serviceIp) {
         try {
-            String data = KeyCache.getData(id);
-            return ResponseEntity.ok(data);
+            PublicKey key = KeyCache.getKeyForService(serviceIp);
+            return ResponseEntity.ok("Public key for service IP " + serviceIp + ": " + key.toString());
         } catch (EncryptionException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error retrieving data: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
         }
     }
 }
-
 
 
