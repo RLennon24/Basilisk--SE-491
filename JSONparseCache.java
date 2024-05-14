a747b19eed7710869cc9169c9d43eeda23fa974a
Riley Lennon <139299781+RLennon24@users.noreply.github.com>
2024-05-14 18:53:19 -0500

    Update and rename JSONparseCache to JSONparseCache.java

---
+++ JSONparseCache.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class ParsingCachingJSON {
+	Gson gson = new Gson();
+  DataUnit data= new DataUnit();
+
+  String fileName= "
+
+  
+	
+	
+	
+	
+}
+
+class DataUnit{
+	private String id;
+	private String data;
+	private int ip;
+	private ArrayList<DataUnit> tags;
+	private ArrayList<DataUnit> roles;
+	
+	
+	public String getId() {return id;}
+	public String getData() {return data;}
+	public int getIp() {return ip;}
+	public ArrayList <DataUnit> getTags() {return tags;}
+	public ArrayList <DataUnit> getRoles(){return roles;}
+	
+	
+}
