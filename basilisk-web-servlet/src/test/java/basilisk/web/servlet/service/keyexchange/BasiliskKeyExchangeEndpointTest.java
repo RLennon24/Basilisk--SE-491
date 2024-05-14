9a9c98db3114f31382b6736cacfab2711748d1b5
Zanco2022 <100976175+Zanco2022@users.noreply.github.com>
2024-05-14 00:29:19 -0700

    Update BasiliskKeyExchangeEndpointTest.java

--- basilisk-web-servlet/src/test/java/basilisk/web/servlet/service/keyexchange/BasiliskKeyExchangeEndpointTest.java
+++ basilisk-web-servlet/src/test/java/basilisk/web/servlet/service/keyexchange/BasiliskKeyExchangeEndpointTest.java
@@ -1,5 +1,6 @@
 package basilisk.web.servlet.service.keyexchange;
-
+import org.mockito.MockedStatic;
+import org.mcokito.Mockito;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -32,9 +33,9 @@ public class BasiliskKeyExchangeEndpointTest {
     public void testExchangeKey() throws Exception {
         try (MockedStatic<KeyCache> mockedKeyCache = Mockito.mockStatic(KeyCache.class)) {
             PublicKey mockPublicKey = mock(PublicKey.class);
-            mockedKeyCache.when(() -> KeyCache.getKeyForService("client")).thenReturn(mockPublicKey);
-        String requestBody = "{\"publicKey\":\"ClientPublicKey\"}";
-
+            mockedKeyCache.when(() -> KeyCache.getKeyForService("client")).thenReturn(mockPublicKey);    
+            String requestBody = "{\"publicKey\":\"ClientPublicKey\"}";
+    
         //Simulates a POST request to the api/exchange-keys
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/exchange-keys")
@@ -42,19 +43,19 @@ public class BasiliskKeyExchangeEndpointTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.serverPublicKey").exists());
-    }
+        }
     }
     //Test method for the GET endpoint, then it expects a succesful response status. Then it verifies that the returned content matches "The data for ID: 1"
     @Test
     public void getTestData() throws Exception {
-	PublicKey mockPublicKey = mock(PublicKey.class);
-	mockedKeyCache.when(() -> KeyCache.getKeyForService("client")).thenReturn(mockPublicKey);
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api/data/1")
-                        .accept(MediaType.APPLICATION_JSON))
+        try (MockedStatic<KeyCache> mockedKeyCache = Mockito.mockStatic(KeyCache.class)) {
+            PublicKey mockPublicKey = mock(PublicKey.class);
+            mockedKeyCache.when(() -> KeyCache.getKeyForService("client")).thenReturn(mockPublicKey);
+    
+            mockMvc.perform(MockMvcRequestBuilders.get("/api/data/1")
+                    .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string("The data for ID: 1"));
-
+                .andExpect(content().string("The data for ID: 1 with the key: " + mockPublicKey.toString()));
+        }
     }
 }
-
