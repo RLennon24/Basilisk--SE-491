f32c0054217e10aedaaef54a50c8995584e5e213
STraenkle <ssamowa3l@yahoo.com>
2024-05-14 22:54:13 -0500

    progress, nothing works yet

--- basilisk-user-servlet/src/test/java/basilisk/UserServletAppTest.java
+++ basilisk-user-servlet/src/test/java/basilisk/UserServletAppTest.java
@@ -1,8 +1,11 @@
 package basilisk;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.util;
+import org.springframework.test.web.servlet.MockMvc;
 
 public class UserServletAppTest {
+
+    @Autowired
+    private MockMvc mockMvc;    
 
 }
