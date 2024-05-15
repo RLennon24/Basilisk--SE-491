7d6f07fbf0b24e1d90f18d0164d8ec53f62c716f
Riley Lennon <139299781+RLennon24@users.noreply.github.com>
2024-05-14 20:12:44 -0500

    Update and rename JSONparseCache.java to JsonParseCache.java

---
+++ dataparser/JsonParseCache.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class JsonParseCache {
+		//read in as string
+		String file= "dataparser/JSONdata.json";
+		String json= readFileAsString(file);
+		
+		//get data --> Java Object
+		Gson gson= new Gson();
+		DataUnit DataUnit = gson.fromJson(json,DataUnit.getClass());
+		
+		
+	
+
+	private static String readFileAsString(String file) throws IOException {
+		return new String (Files.readAllBytes(Paths.get(file)));
+	}
+
+}
