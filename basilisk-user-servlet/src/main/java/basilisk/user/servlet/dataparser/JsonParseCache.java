b63030e46649b42095dd0c4647f09c3fee30196c
Riley Lennon <139299781+RLennon24@users.noreply.github.com>
2024-05-14 21:45:18 -0500

    Update JsonParseCache.java

--- basilisk-user-servlet/src/main/java/basilisk/user/servlet/dataparser/JsonParseCache.java
+++ basilisk-user-servlet/src/main/java/basilisk/user/servlet/dataparser/JsonParseCache.java
@@ -15,7 +15,7 @@ public class JsonParseCache {
 		DataUnit dataUnit = gson.fromJson(json,DataUnit.getClass());
 		
 	
-	private static String readFileAsString(String file) throws IOException {
+	private String readFileAsString(String file) throws IOException {
 		return new String (Files.readAllBytes(Paths.get(file)));
 	}
 
