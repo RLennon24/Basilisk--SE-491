65259c8c268d764b419f4b71ffe7a74297bb274f
Marcel <marcel.alfonso99@gmail.com>
2024-05-15 18:30:33 -0500

    BAS-8: Working message exchange

--- basilisk-user-servlet/src/main/java/basilisk/user/servlet/service/KeyExchangeService.java
+++ basilisk-user-servlet/src/main/java/basilisk/user/servlet/service/KeyExchangeService.java
@@ -1,16 +1,14 @@
 package basilisk.user.servlet.service;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/key-exchange")
 
-public class KeyExchangeService {
+public class  KeyExchangeService {
 
-    @RequestMapping(value = "/public-key", method = RequestMethod.POST)
+    @PostMapping(value = "/public-key")
     @ResponseBody
     public String publicKey() {
         return "public-key";
