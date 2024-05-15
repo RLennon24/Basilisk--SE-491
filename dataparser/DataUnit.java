3f53bd3947e582e0f5bf15fd94aa6c031f186776
Riley Lennon <139299781+RLennon24@users.noreply.github.com>
2024-05-14 20:11:53 -0500

    Create DataUnit.java

---
+++ dataparser/DataUnit.java
@@ -0,0 +1,16 @@
+Public class DataUnit{
+	private String id;
+	private String data;
+	private int ip;
+	private ArrayList<DataUnit> tags;
+	private ArrayList<DataUnit> roles;
+	
+	
+	public String getId() {return id;}
+	public String getData() {return data;}
+	public int getIp() {return ip;}
+	public ArrayList <DataUnit> getTags() {return tags;}
+	public ArrayList <DataUnit> getRoles(){return roles;}
+	
+	
+}
