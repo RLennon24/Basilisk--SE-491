57fd93c70ec8ae21c82f9e9210a52584ddcc3070
Riley Lennon <139299781+RLennon24@users.noreply.github.com>
2024-05-15 20:48:42 -0500

    Update DataUnit.java

--- basilisk-user-servlet/src/main/java/basilisk/user/servlet/dataparser/DataUnit.java
+++ basilisk-user-servlet/src/main/java/basilisk/user/servlet/dataparser/DataUnit.java
@@ -1,30 +1,30 @@
 import java.util.ArrayList;
 
-public class DataUnit{
-	private String id;
-	private String data;
-	private int ip;
-	private ArrayList<String> tags;
-	private ArrayList<String> roles;
+public class DataUnit {
+		private String id;
+		private String data;
+		//private int ip;
+		private ArrayList <String> tags;
+		private ArrayList <String> roles;
 
-	public DataUnit(){
+		public DataUnit(){
+			
+		}
+
+		public DataUnit(String id, String data, ArrayList<String> tags, ArrayList<String> roles){
+			this.id= id;
+			this.data=data;
+			//this.ip= ip;
+			this.tags= tags;
+			this.roles= roles;
+		}
+		
+		
+		public String getId() {return id;}
+		public String getData() {return data;}
+		//public int getIp() {return ip;}
+		public ArrayList <String> getTags() {return tags;}
+		public ArrayList <String> getRoles(){return roles;}
+		
 		
 	}
-
-	public DataUnit(String id, String data, int ip, String[] tags, String[] roles){
-		this.id= id;
-		this.data=data;
-		this.ip= ip;
-		this.tags= tags;
-		this.roles= roles;
-	}
-	
-	
-	public String getId() {return id;}
-	public String getData() {return data;}
-	public int getIp() {return ip;}
-	public ArrayList <DataUnit> getTags() {return tags;}
-	public ArrayList <DataUnit> getRoles(){return roles;}
-	
-	
-}
