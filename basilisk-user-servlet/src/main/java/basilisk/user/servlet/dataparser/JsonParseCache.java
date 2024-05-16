36521e2e56e9ce6ccff8550a0a4fd867cbb9556f
Riley Lennon <139299781+RLennon24@users.noreply.github.com>
2024-05-15 20:49:27 -0500

    Update JsonParseCache.java

--- basilisk-user-servlet/src/main/java/basilisk/user/servlet/dataparser/JsonParseCache.java
+++ basilisk-user-servlet/src/main/java/basilisk/user/servlet/dataparser/JsonParseCache.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -10,11 +13,13 @@ public class JsonParseCache {
 	public void ParseJSON(){
 		//read in as string
 		try{
-			BufferedReader reader= Files.newBufferedReader(Paths.get("basilisk-user-servlet/src/main/java/basilisk/user/servlet/dataparser/DataUnit.java"));
-		
-		//get data --> Java Object
-		Gson gson= new Gson();
+			Gson gson= new Gson();
+			BufferedReader reader= Files.newBufferedReader(Paths.get("/Basilisk/src/data/sampl.json"));
+			
+		//get data --> Java Object --> store in map
+		Map<String, JsonParseCache.DataUnit> map= new HashMap<String,DataUnit>();
 		DataUnit dataUnit = gson.fromJson(reader,DataUnit.class);
+		map= (Map<String,DataUnit>)gson.fromJson(reader, DataUnit.class);
 		
 		}catch (Exception e) {
 			e.printStackTrace();
