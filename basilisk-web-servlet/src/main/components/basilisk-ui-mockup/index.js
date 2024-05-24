bbf1400cd09df74b24da0bf94a09f2aed1f02dbe
Maheen Khan <mkhan238@depaul.edu>
2024-05-23 20:12:59 -0500

    Moved UI files for starter code under source
    also changed paths in index.js so it works

--- basilisk-web-servlet/src/main/components/basilisk-ui-mockup/index.js
+++ basilisk-web-servlet/src/main/components/basilisk-ui-mockup/index.js
@@ -5,7 +5,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css'; // Import CSS for styling
-import BasiliskDashboard from './components/basilisk-ui-mockup/basilisk-dashboard';
+import BasiliskDashboard from './basilisk-dashboard';
 
 
 // Render the BasiliskDashboard component to the root element in the HTML document
