6a548fe2a866a250c0505333083fe41eabb10162
Riley Lennon <139299781+RLennon24@users.noreply.github.com>
2024-05-27 16:30:42 -0500

    Update idgeneration.java

--- basilisk-web-servlet/src/main/java/basilisk/web/servlet/idgen/idgeneration.java
+++ basilisk-web-servlet/src/main/java/basilisk/web/servlet/idgen/idgeneration.java
@@ -1,3 +1,8 @@
+package basilisk.web.servlet.idgen;
+
+import java.util.UUID;
+import basilisk.user.servlet.parsing.DataUnit;
+
 public class idgeneration {
    public static void idgen(DataUnit DataUnit) {
         String strKey = DataUnit.getName() + DataUnit.getDataCreator() + DataUnit.getTimestamp();
