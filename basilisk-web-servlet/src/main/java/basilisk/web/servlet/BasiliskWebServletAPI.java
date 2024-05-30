7984190f7e47eba9a1be9ea9cad298ac6feb5893
zach <zwile@depaul.edu>
2024-05-30 08:48:28 +0000

    added comments to user

--- basilisk-web-servlet/src/main/java/basilisk/web/servlet/BasiliskWebServletAPI.java
+++ basilisk-web-servlet/src/main/java/basilisk/web/servlet/BasiliskWebServletAPI.java
@@ -1,5 +1,4 @@
 package basilisk.web.servlet;
-
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -9,21 +8,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+//These are the necessary HTTP classes and Spring classes
 
 @RestController
 @RequestMapping("/api")
-public class BasiliskWebServletAPI {
 
-    @PostMapping("/insert")
+public class BasiliskWebServletAPI {
+    @PostMapping("/insert") //This is the POST method, which is here to send data to the user servlet
+
     public ResponseEntity<String> sendDataToUserServlet(@RequestBody String dataJson) {
-        String userServletUrl = "http://localhost:8001/insert";
-        RestTemplate restTemplate = new RestTemplate();
+        String userServletUrl = "http://localhost:8001/insert"; //This is the URL of the User Servlet
+        RestTemplate restTemplate = new RestTemplate(); //Creates a RestTemplate instance. This is for sending HTTP requests
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> request = new HttpEntity<>(dataJson, headers);
         
         // Sending data to the User Servlet
         String response = restTemplate.postForObject(userServletUrl, request, String.class);
-        return ResponseEntity.ok("Data forwarded to User Servlet and received response: " + response);
+        return ResponseEntity.ok("Data successfully reached User Servlet and got a response: " + response); //This is to indicate success from the User servlet
     }
 }
