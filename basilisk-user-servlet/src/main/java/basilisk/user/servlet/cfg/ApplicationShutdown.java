56a84f2f246637fe033e50ec139f077d3a61981c
Marcel <marcel.alfonso99@gmail.com>
2024-06-07 18:56:17 -0500

    BAS-24: Gracious shutdown, removes keys in web servlet on shutdown

---
+++ basilisk-user-servlet/src/main/java/basilisk/user/servlet/cfg/ApplicationShutdown.java
@@ -0,0 +1,29 @@
+package basilisk.user.servlet.cfg;
+
+import basilisk.user.servlet.client.WebServletClient;
+import basilisk.user.servlet.keyexchange.packaging.KeyPackager;
+import basilisk.user.servlet.keygen.BasiliskUserKeyGen;
+import basilisk.user.servlet.message.BaseMessageBuilder;
+import basilisk.user.servlet.parsing.JsonParseCache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class ApplicationShutdown implements ApplicationListener<ContextClosedEvent> {
+
+    @Autowired
+    Environment environment;
+
+     @Override
+     public void onApplicationEvent(ContextClosedEvent event) {
+         // TODO: send to server a de-listing notif
+         RestTemplate template = new RestTemplate();
+         WebServletClient.sendRequest(template, environment.getProperty("account.owner"), environment.getProperty("server.delist.url"), BaseMessageBuilder.encodeMessage("DELIST"));
+         BasiliskUserKeyGen.storeUserKeys();
+         JsonParseCache.writeToFiles();
+     }
+}
--- basilisk-user-servlet/src/main/java/basilisk/user/servlet/keyexchange/KeyExchangeClient.java
+++ basilisk-user-servlet/src/main/java/basilisk/user/servlet/keyexchange/KeyExchangeClient.java
@@ -1,5 +1,6 @@
 package basilisk.user.servlet.keyexchange;
 
+import basilisk.user.servlet.client.WebServletClient;
 import basilisk.user.servlet.encryption.EncrypterUtil;
 import basilisk.user.servlet.exception.EncryptionException;
 import basilisk.user.servlet.keyexchange.packaging.KeyPackager;
@@ -27,7 +28,7 @@ public class KeyExchangeClient {
             System.out.println("Sending Public Key to Web Server for Public Key Exchange");
 
             // send public key to server and await for response
-            BaseMessage publicKeyPackage = sendKeyRequest(template, owner, keyExchangeEndpoint + "/publicKey", KeyPackager.generatePublicKeyTransport());
+            BaseMessage publicKeyPackage = WebServletClient.sendRequest(template, owner, keyExchangeEndpoint + "/publicKey", KeyPackager.generatePublicKeyTransport());
             KeyUnpackager.processPublicKeyPackage(publicKeyPackage);
 
             if (KeyCache.getServerPublicKey() == null) {
@@ -37,25 +38,12 @@ public class KeyExchangeClient {
             System.out.println("Successfully exchanged public keys with the Server");
             System.out.println("Attempting to exchange symmetric keys with the Server");
 
-            BaseMessage symmetricKeyPackage = sendKeyRequest(template, owner, keyExchangeEndpoint + "/symmetricKey", KeyPackager.generateSymmetricKeyTransport());
+            BaseMessage symmetricKeyPackage = WebServletClient.sendRequest(template, owner, keyExchangeEndpoint + "/symmetricKey", KeyPackager.generateSymmetricKeyTransport());
             System.out.println("Server: " + EncrypterUtil.decodeMessage(symmetricKeyPackage));
         } catch (Exception e) {
             throw new EncryptionException("Could not exchange keys. Closing connection");
         }
     }
 
-    private static BaseMessage sendKeyRequest(RestTemplate template, String owner, String endpoint, BaseMessage keyTransport) {
-        HttpEntity<BaseMessage> request = new HttpEntity<>(keyTransport, createHeaders(owner));
-        ResponseEntity<BaseMessage> responseEntity = template.postForEntity(endpoint, request, BaseMessage.class);
-        final BaseMessage keyPackage = Optional.ofNullable(responseEntity.getBody()).orElseThrow(() -> new IllegalArgumentException("Response cannot be null"));
-        return keyPackage;
-    }
 
-    private static MultiValueMap<String, String> createHeaders(String owner) {
-        MultiValueMap<String, String> defaultHeaders = new LinkedMultiValueMap<>();
-        defaultHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        defaultHeaders.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
-        defaultHeaders.add("DATA-OWNER", owner);
-        return defaultHeaders;
-    }
 }
