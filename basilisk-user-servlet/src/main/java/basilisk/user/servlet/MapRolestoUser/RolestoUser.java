553250322e28445f8f605e33a1238959bff77db4
riley <rlennon@depaul.edu>
2024-06-12 20:52:55 -0500

    adding run config and shutdown

--- basilisk-user-servlet/src/main/java/basilisk/user/servlet/MapRolestoUser/RolestoUser.java
+++ basilisk-user-servlet/src/main/java/basilisk/user/servlet/MapRolestoUser/RolestoUser.java
@@ -1,13 +1,13 @@
 package basilisk.user.servlet.MapRolestoUser;
 
 import basilisk.user.servlet.parsing.DataUnit;
+import basilisk.user.servlet.parsing.JsonParseCache;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -15,21 +15,21 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
 
 
 public class RolestoUser {
 
-    private static final Map<String , List<String>> RoletoUser= new ConcurrentHashMap<>();
-    String User= "Maheen";
+    private static Map<String , List<String>> RoletoUser= new ConcurrentHashMap<>();
 
-    //check if list has roles
-    public boolean doesUserHaveRole(String user, String role) throws IOException {
+    //check if user has roles
+    public boolean doesUserHaveRole(String User , String role) throws IOException {
         List <String> listUsers= RoletoUser.get(role);
         if(listUsers==null){
             System.out.println("Role not found");
             return false;
         }
-        if(listUsers.contains(user)){
+        if(listUsers.contains(User)){
             System.out.println("User found with role");
                 return true;
         }else{
@@ -38,11 +38,11 @@ public class RolestoUser {
         }
     }
 
-    public void RoletoUser(DataUnit dataUnit) {
-            //get roles data assign to user
-            for (String role : dataUnit.getRoles()) {
-                RoletoUser.computeIfAbsent(role, s -> Collections.synchronizedList(new ArrayList<>())).add(User);
-            }
+    //insert data
+    public void giveUserRole(String User,String role) {
+            RoletoUser.computeIfAbsent(role, s -> Collections.synchronizedList(new ArrayList<>()));
+            RoletoUser.get(role).add(User);
+
     }
 
     public static void writeFiles(){
@@ -50,15 +50,33 @@ public class RolestoUser {
         Gson gson= new Gson();
         Type typeObject= new TypeToken<Map>(){}.getType();
         String gsonData = gson.toJson(RoletoUser,typeObject);
-
-        //save to file
         Path folderPath= Paths.get(System.getProperty("user.home") + File.separator +
                 "basilisk" + File.separator + "RolestoUserJSON");
+
+        //save to file
         try (FileWriter jsonFile= new FileWriter((folderPath.toFile()))){
             jsonFile.write(gsonData);
         }catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    //read from file
+    public static void readFromFiles() {
+        Path folderPath= Paths.get(System.getProperty("user.home") + File.separator +
+                "basilisk" + File.separator + "RolestoUserJSON");
+
+        System.out.println("Fetching Roles/User files in: " + folderPath.toAbsolutePath());
+
+        Map<String , List<String>> rolesFromFiles= new ConcurrentHashMap<>();
+        Gson gson= new Gson();
+        try{
+            RoletoUser = gson.fromJson(new FileReader(folderPath.toFile()), new TypeToken<Map<String, Object>>() {}.getType());
+            System.out.println("Read roles/user data from file");
+        }catch (IOException e) {
+            throw new RuntimeException("Could not find data at: " + folderPath.toAbsolutePath());
+        }
+
     }
 }
 
